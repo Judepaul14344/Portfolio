0fe82ffcb700f84cc366619d77c540a2ea886f54
judepaul14344 <judepaul14344@gmail.com>
2024-04-06 23:57:58 +0800

    first commit

---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  setup() {
+    return {
+      bios: [
+        {
+          id: 1,
+          bio: "Hello, I'm Jude Paul—a melody enthusiast with a heart for technology and a soft spot for our furry friends. Singing and playing instruments are my sanctuary, where I pour my emotions into every note, while caring for and feeding dogs brings me boundless joy and fulfillment. Alongside my passion for music and canine companionship, I'm carving my path as an aspiring IT student, diving into the depths of coding and networking with eagerness and curiosity. Each strum of a chord and each line of code written marks a step forward on my journey of self-discovery and growth, shaping me into a harmonious blend of creativity and technical expertise.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+    <!-- About profile image -->
+    <div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+      <img src="@/assets/images/profile.jpg" class="rounded-xl w-96" alt="" />
+    </div>
+
+    <!-- About details -->
+    <div class="w-full sm:w-3/4 text-left">
+      <p
+        v-for="bio in bios"
+        :key="bio.id"
+        class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+      >
+        {{ bio.bio }}
+      </p>
+    </div>
+  </div>
+</template>
---
+++ src/data/socialLinks.js
@@ -0,0 +1,20 @@
+export const socialLinks = [
+  {
+    id: 1,
+    name: "Website",
+    icon: "facebook",
+    url: "https://www.facebook.com/paul.jude.5811",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    icon: "github",
+    url: "https://github.com/Judepaul14344",
+  },
+  {
+    id: 3,
+    name: "Instagram",
+    icon: "instagram",
+    url: "https://www.instagram.com/judepaui/",
+  },
+];
